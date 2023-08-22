f166a714ef01b8cd82955e48dab87a68e00933ae
Iordache Catalin <iordache.catalin1986@gmail.com>
2023-08-23 01:32:46 +0300

    Up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter  as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Contact from "../src/Pages/Contact/Contact.js";
